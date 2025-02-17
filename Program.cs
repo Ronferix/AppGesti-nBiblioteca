12e1c2bf0bc7b85b10a4747528cfc568d29bb0c1
Ronferix <gilbertoatzayacatl1@gmail.com>
2025-02-16 22:04:48 -0600

    Working on function to edit Book's info
    I was working on the function desingned to change the information of the book. It should be used in at least 2 diferent options of the main menu (main menu is not created yet, only the object 'book', the functions GetGenresFromUser, AddBook are completed and the function EditBook is half way done)

--- Program.cs
+++ Program.cs
@@ -26,7 +26,6 @@ public class Book
 
     }
 
-
     public void ShowBookInfo()
     {
         Console.WriteLine($@"Book Information:\n" +
@@ -67,7 +66,7 @@ class AppGestiónBiblioteca
             Console.Write("Enter year of publication (use numbers): ");
             string getYear = Console.ReadLine();
             
-            if (int.TryParse(getYear, out year))
+            if (!string.IsNullOrWhiteSpace(getYear) && int.TryParse(getYear, out year))
             {
                 break;
             }
@@ -112,9 +111,91 @@ class AppGestiónBiblioteca
 
     }
 
-    public static void EditBook()
+    public static void EditBook(Book bookToEdit)
     {
+        while (true) { 
+            Console.WriteLine("Select one option to edit (use numbers to select the option):\n" +
+                "1. Title.\n" +
+                "2. Author.\n" +
+                "3. Year.\n" +
+                "4. Description.\n" +
+                "5. Copies.\n" +
+                "6. Exit editor.\n");
 
+            int selection;
+            while (true)
+            {
+                Console.Write("Option: ");
+                string getSelection = Console.ReadLine();
+
+                if (!string.IsNullOrWhiteSpace(getSelection) && int.TryParse(getSelection, out selection))
+                {
+                    break;
+                }
+                Console.WriteLine("Error! incorrect input, use numbers");
+            }
+
+            string input;
+            int inputToInt;
+            switch (selection){
+                case 1:
+                    Console.WriteLine($"The actual title is: {bookToEdit.Title}\nEnter the new title: ");
+                    input = Console.ReadLine();
+                    bookToEdit.Title = input;
+                    input = "";
+                    break;
+                case 2:
+                    Console.WriteLine($"The actual author is: {bookToEdit.Author}\nEnter the new author: ");
+                    input = Console.ReadLine();
+                    bookToEdit.Author = input;
+                    input = "";
+                    break;
+                case 3:
+                    while (true)
+                    {
+                        Console.Write($"The actual year is: {bookToEdit.YearOfPublication}\nEnter the new year (use numbers): ");
+                        input = Console.ReadLine();
+                        if (!string.IsNullOrWhiteSpace(input) && int.TryParse(input, out inputToInt))
+                        {
+                            break;
+                        }
+
+                        Console.WriteLine("Error! incorrect input, use numbers");
+                    }
+                    bookToEdit.YearOfPublication = inputToInt;
+                    input = "";
+                    inputToInt = 0;
+                    break;
+                case 4:
+                    Console.WriteLine($"The actual description is: {bookToEdit.Description}\nEnter the new description: ");
+                    input = Console.ReadLine();
+                    bookToEdit.Description = input;
+                    input = "";
+                    break;
+                case 5:
+                    while (true)
+                    {
+                        Console.WriteLine($"The actual number of copies is: {bookToEdit.Copies}\nEnter the new number of copies (use numbers): ");
+                        input = Console.ReadLine();
+                        if (!string.IsNullOrWhiteSpace(input) && int.TryParse(input, out inputToInt))
+                        {
+                            break;
+                        }
+
+                        Console.WriteLine("Error! incorrect input, use numbers");
+                    }
+                    bookToEdit.Copies = inputToInt;
+                    input = "";
+                    inputToInt = 0;
+                    break;
+                case 6:
+                    Console.WriteLine("Ending edition...");
+                    return;
+                default:
+                    Console.WriteLine("Unknown ERROR - Ending edition...");
+                    return;
+            }
+        }
     }
 
     static List<string> GetGenresFromUser()
